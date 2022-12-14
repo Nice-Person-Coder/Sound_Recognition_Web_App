320fedfb6ce4fcff78657092eb0f8b49caca3afc
Nice-Person-Coder <110208689+Nice-Person-Coder@users.noreply.github.com>
2022-12-13 19:52:00 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function StartClassification()
+{
+navigator.mediaDevices.getUserMedia({ audio: true});
+classifier= ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/EunPmv8Dn/model.json',modelReady);
+}
+
+function modelReady(){
+    classifier.classify(gotResults);
+}
